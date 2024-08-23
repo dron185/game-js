0bfab1b5fa5edddc097aae533dac7be7aa7f621c
Andrei Huryn <gurin.nord@gmail.com>
2024-08-23 21:18:24 +0300

    add one more test

--- game.js
+++ game.js
@@ -1,6 +1,27 @@
 export class Game {
-    test(){
-        return 'test'
+    #state = GAME_STATUSES.PENDING
+    #googlePosition = {x: 1, y: 1}
+
+    #jumpGoogle() {
+        this.#googlePosition = {x: 2, y: 3}
     }
-    //constructor(game) {}
+
+    async getStatus() {
+        return this.#state
+    }
+
+    async start() {
+        setInterval(this.#jumpGoogle.bind(this), 2000)
+        this.#state = GAME_STATUSES.IN_PROGRESS
+    }
+
+    async getGooglePosition() {
+        return this.#googlePosition
+    }
+}
+
+export const GAME_STATUSES = {
+    PENDING: 'PENDING',
+    IN_PROGRESS: 'iN-PROGRESS',
+    FINISHED: 'FINISHED'
 }
--- game.test.js
+++ game.test.js
@@ -1,23 +1,34 @@
-import {Game} from "./game";
+import {Game, GAME_STATUSES} from "./game";
 
 describe("Game Tests", () => {
 
-    it('should return gridSize', () => {
+    it("should return correct Game Status 'STARTED' after start", async () => {
         const game = new Game();
 
-        game.setSettings(
-            {
-                gridSize: {
-                    columns: 10,
-                    rows: 10,
-                }
-            }
-        )
+        let status = await game.getStatus()
 
-        const settings = game.getSettings()
+        expect(status).toBe(GAME_STATUSES.PENDING);
 
-        expect(settings.gridSize.rows).toBe(10);
-        expect(settings.gridSize.columns).toBe(10);
+        await game.start();
+        status = await game.getStatus()
+        expect(status).toBe(GAME_STATUSES.IN_PROGRESS);
+
+    });
+
+    it("should return correct Google position that changed every 2 seconds", async () => {
+        const game = new Game();
+        await game.start();
+
+        let googlePosition = await game.getGooglePosition()
+        expect(googlePosition).toBeDefined();
+
+        await delay(3000)
+
+        let googlePosition2 = await game.getGooglePosition()
+        expect(googlePosition2).not.toEqual(googlePosition)
     });
 
 })
+
+export const delay = (ms) =>
+    new Promise(resolve => setTimeout(resolve, ms))
